13fbdce8801268495401db1694f7b1cec8d99547
Andichi, Naoufal - Student Rijn IJssel <00313992@rijnijssel.nl>
2025-02-22 15:24:33 +0100

    push

--- controllers/home.php
+++ controllers/home.php
@@ -459,7 +459,7 @@ require_once 'views/templates/header.php';
                                     <svg class="w-5 h-5 text-primary mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                                     </svg>
-                                    <span>Slechts 5-10 minuten</span>
+                                    <span>Slechts 10-15 minuten</span>
                                 </div>
                                 <div class="flex items-center text-gray-600">
                                     <svg class="w-5 h-5 text-primary mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
