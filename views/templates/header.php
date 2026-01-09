3d223b69d8526b3189eb22ead00cbe0e8b38f117
Andichi, Naoufal - Student Rijn IJssel <00313992@rijnijssel.nl>
2026-01-09 16:09:13 +0100

    Add Google Analytics tracking to header template
    - Integrate Google tag (gtag.js) for enhanced tracking and analytics capabilities.
    - Set up initial configuration for tracking page views and user interactions.
    - Ensure the script is loaded asynchronously to optimize page performance.

--- views/templates/header.php
+++ views/templates/header.php
@@ -248,6 +248,15 @@ if ($currentPage === 'amerikaanse-verkiezingen') {
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XLQMYP3CDD"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-XLQMYP3CDD');
+    </script>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
